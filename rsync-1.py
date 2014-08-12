75021e9a3bcd0ad0ed4aa56f7e4eb6b48df8e1f6
John Hoe <john.hoe@gmail.com>
2014-08-13 03:40:19 +0800

    python rsync script, translated from a previous bash script, except error handling not working

---
+++ rsync-1.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+#translation of a bash script used previously.
+
+import os, subprocess
+
+useraccount = "user1"
+source_path = "/source_path/"
+destination_path = "/destination_path/"
+destination_ip = ["127.0.0.1","127.0.0.1","127.0.0.1","127.0.0.1"]
+exclude_element = "dir1"
+
+#check if source path is available
+def _check_paths_(useraccount, source_path, destination_path, destination_ip, exclude_element):
+	sp = os.path.exists(source_path)
+	if sp == True:
+		_syncthemall_(useraccount, source_path, destination_path, destination_ip, exclude_element)
+	else:
+		return "\"%s\" cannot be found." % source_path
+
+#rsync process begins here, "except OSError doesn't work, gotta work on it"
+def _syncthemall_(useraccount, source_path, destination_path, destination_ip, exclude_element):
+	for d_ip in destination_ip:
+		print "############ transferring to %s ############" % d_ip
+		rsync_args = ["/usr/bin/rsync", "--exclude="+str(exclude_element), "-ave", "ssh", str(source_path), str(useraccount+"@"+d_ip+":"+destination_path)]
+		print "executing " + " ".join(rsync_args)
+		try:
+			subprocess.call(rsync_args)
+		except OSError as (errno, strerror):
+			print "%d, %s" % (errno, strerror)
+
+_check_paths_(useraccount, source_path, destination_path, destination_ip, exclude_element)
