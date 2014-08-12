e559270c6cbec688eed4cdc80c19251dc50656d3
John Hoe <john.hoe@gmail.com>
2014-08-12 22:03:39 +0800

    python script to check if file/dir exists, if exist, stat it.

---
+++ stat-filesordirs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+
+path = raw_input("Enter a absolute path to a directory or a file.\
+	E.g. \"/bin/\" or \"/bin/chmod\"")
+
+def statfile(path):
+	try:
+		file_stat_info = os.stat(path)
+		return file_stat_info
+	except OSError:
+		print "OS error({0}): {1}" .format(OSError.errno, OSError.strerror)
+	except:
+		print "Unexpected error:", os.exc_info()[0]
+
+print statfile(path)
