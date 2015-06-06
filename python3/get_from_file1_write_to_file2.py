348443f8af0ac7557ab8e7fa257b72ee2daf2627
john hoe <john.hoe@gmail.com>
2015-06-07 01:18:33 +0800

    Right, finally got it to work. (Y)

--- python3/get_from_file1_write_to_file2.py
+++ python3/get_from_file1_write_to_file2.py
@@ -5,7 +5,7 @@ import argparse
 from time import strftime
 
 def checkfiles(file1, file2):
-    """Simple check on whether the files exists. file1 should exist, file2 should not."""
+    """Simple check on whether the files exists. file1 should exist, file2 should not. If file2 exists, we can choose to overwrite it, or exit."""
 
     files = [file1, file2]
     try:
@@ -28,15 +28,9 @@ def getoccurencesfromfile(file1, file2, svctime):
     """If the service time(svctime), column 4, from file is greater than a certain amount, copy them out into file2."""
 
     files = checkfiles(file1, file2)
-    
-    print(files)
-    print(files[0])
-    print(type(files[0]))
-    print(files[1])
-    print(type(files[1]))
 
-    file1_read = os.open(files[0], "r")
-    file2_write = os.open(files[1], "w")
+    file1_read = open(files[0], "r")
+    file2_write = open(files[1], "w")
 
     getline = file1_read.readlines()
     for i in getline:
@@ -45,15 +39,12 @@ def getoccurencesfromfile(file1, file2, svctime):
     file2_write.close()
     file1_read.close()
 
-#def changefilename(file2):
-#    datum = strftime("%Y-%m-%d_%H:%M:%S") #E.g. '2015-06-06_23:14:17'
-#    file2a = os.path.split(file2)[0] #E.g. '('/path/to', 'file.ext')' or ('', 'file.ext')
-#    file2b = os.path.split(file2)[1]
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("file1", help="Name of the first file (absolute or relative path) you are checking against.")
     parser.add_argument("file2", help="Name of the second file (absolute or relative path) which will be populated with the results.")
     parser.add_argument("seconds", type=float, help="Amount of time, in seconds, which will be matched against the service time column, column 4, from file1.")
     args = parser.parse_args()
+    #print("{0} --- {1} --- {2}".format(args.file1, args.file2, args.seconds))
+
     getoccurencesfromfile(args.file1, args.file2, args.seconds)
