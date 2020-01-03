2346f0c6ebbad085e5ac62bf88e8ced28effd7ed
John <jhn--@users.noreply.github.com>
2020-01-03 15:28:36 +0800

    Delete generate_firebase_token.py

--- python3/generate_firebase_token.py
+++
@@ -1,5 +0,0 @@
-from firebase_token_generator import create_token
-
-auth_payload = { "uid": "custom:1", "auth_data": "foo", "other_auth_data": "bar" }
-token = create_token("CWO3CaLOZcjWr2jzVOstH3q6PAHPzXcKXsJcQOAb", auth_payload)
-print(token)
